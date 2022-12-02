c8784d23b72bcad4d47b6929c44298f3ab5fac35
Nikita Raven <esh2015@list.ru>
2022-12-02 20:15:11 +0400

    Claass Example

--- main.py
+++ main.py
@@ -2,3 +2,26 @@ from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QMessageBox, QDe
 from PyQt5.QtGui import QIcon
 
 import sys
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.resize(250, 150)
+        self.center()
+
+        self.setWindowTitle("Icon")
+        self.setWindowIcon(QIcon("dollar.jpg"))
+
+        self.show()
+
+    def closeEvent(self, event):
+
+        reply = QMessageBox.question(self, "Message",
+                                     "Are you sure to quit?", QMessageBox.Yes |
+                                     QMessageBox.No, QMessageBox.No)
